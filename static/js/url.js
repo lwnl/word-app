02690d27469259f068b606f4cc94adbd4876b3ef
Liang_Wang <liangwang.fr@gmail.com>
2024-10-19 18:52:17 +0200

    update

--- static/js/url.js
+++ static/js/url.js
@@ -1,5 +1,5 @@
-const PORT = 8443
+// const PORT = 8443
 let hostIp = 'https://localhost'
 hostIp = 'https://wordapp-test.liangw.de'
 
-export const HOST = `${hostIp}:${PORT}`
+export const HOST = `${hostIp}`
